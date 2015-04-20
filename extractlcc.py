3115abdd33312473ac441825a7410aa7c919c90a
MarkEEaton <mark.eaton@kbcc.cuny.edu>
2015-04-20 22:00:08 +0000

    replaced tabs with 4 spaces

--- extractlcc.py
+++ extractlcc.py
@@ -5,42 +5,42 @@ import pdb
 
 def extract(jsontxt, radiochoice):
 	
-	# set default values of variables
-	changed = []
-	errorcheck = False
-	print "running extract"
+    # set default values of variables
+    changed = []
+    errorcheck = False
+    print "running extract"
 
-	# parse data based on radio button selected. Returns error if it fails.
-	try:
-		if radiochoice == "lcc":
-			choicedata = (jsontxt['SEGMENTS']['JAGROOT']['RESULT']['FACETLIST']['FACET'][1]['FACET_VALUES'])
-			errorcheck = True
-		elif radiochoice == "date":
-			choicedata = (jsontxt['SEGMENTS']['JAGROOT']['RESULT']['FACETLIST']['FACET'][8]['FACET_VALUES'])
-			errorcheck = True
-		elif radiochoice == "genre":
-			choicedata = (jsontxt['SEGMENTS']['JAGROOT']['RESULT']['FACETLIST']['FACET'][9]['FACET_VALUES'])
-			errorcheck = True
-		else:
-			print 'No choice selected'
-			choicedata = {}
-			errorcheck = False 
-	except:
-		print('Term not found')
-		errorcheck = False
+    # parse data based on radio button selected. Returns error if it fails.
+    try:
+        if radiochoice == "lcc":
+            choicedata = (jsontxt['SEGMENTS']['JAGROOT']['RESULT']['FACETLIST']['FACET'][1]['FACET_VALUES'])
+            errorcheck = True
+        elif radiochoice == "date":
+            choicedata = (jsontxt['SEGMENTS']['JAGROOT']['RESULT']['FACETLIST']['FACET'][8]['FACET_VALUES'])
+            errorcheck = True
+        elif radiochoice == "genre":
+            choicedata = (jsontxt['SEGMENTS']['JAGROOT']['RESULT']['FACETLIST']['FACET'][9]['FACET_VALUES'])
+            errorcheck = True
+        else:
+            print 'No choice selected'
+            choicedata = {}
+            errorcheck = False 
+    except:
+        print('Term not found')
+        errorcheck = False
 
-	# change the keys in the json to what is needed by the d3 script
-	def changeKeys(originalJSON):
-		for line in originalJSON:
-			tr = {'@KEY':'name', '@VALUE':'size'}
-			changed.append({tr[k]: v for k, v in line.items()})
-			print "running change keys"
+    # change the keys in the json to what is needed by the d3 script
+    def changeKeys(originalJSON):
+        for line in originalJSON:
+            tr = {'@KEY':'name', '@VALUE':'size'}
+            changed.append({tr[k]: v for k, v in line.items()})
+            print "running change keys"
 
-	# if error free, return use-ready json, else return False
-	if errorcheck == True:
-		changeKeys(choicedata);
-		output = {"name": "content", "children": changed}
-		return output
-	else:
-		print('returning false')
-		return False
+    # if error free, return use-ready json, else return False
+    if errorcheck == True:
+        changeKeys(choicedata);
+        output = {"name": "content", "children": changed}
+        return output
+    else:
+        print('returning false')
+        return False
--- viz.py
+++ viz.py
@@ -15,39 +15,39 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-	# use regex to validate user input
-	def checkstr(userinput, search=re.compile(r'[^ a-zA-Z]').search):
-		return not bool (search(userinput))
+    # use regex to validate user input
+    def checkstr(userinput, search=re.compile(r'[^ a-zA-Z]').search):
+        return not bool (search(userinput))
 
-	# get data from form in viz.html and check using checkstr()
-	if checkstr(request.form['query']) == True:
-		query = request.form['query']
-		print 'checkstr is True'
-	else:
-		print 'checkstr is False'
-		return redirect('/error2')
-	choice = request.form['type']
+    # get data from form in viz.html and check using checkstr()
+    if checkstr(request.form['query']) == True:
+        query = request.form['query']
+        print 'checkstr is True'
+    else:
+        print 'checkstr is False'
+        return redirect('/error2')
+    choice = request.form['type']
 	
-	# make an api request using the inserting the query variable in the url
-	resp = requests.get('http://onesearch.cuny.edu/PrimoWebServices/xservice/search/brief?&institution=KB&onCampus=false&query=any,contains,%s&query=facet_rtype,exact,books&indx=1&lang=eng&json=true' % query)
+    # make an api request using the inserting the query variable in the url
+    resp = requests.get('http://onesearch.cuny.edu/PrimoWebServices/xservice/search/brief?&institution=KB&onCampus=false&query=any,contains,%s&query=facet_rtype,exact,books&indx=1&lang=eng&json=true' % query)
 	
-	# assign the api data to a variable, pass it to the parsing function
-	apicall = json.loads(resp.text)
-	readydata = extractlcc.extract(apicall, choice)
+    # assign the api data to a variable, pass it to the parsing function
+    apicall = json.loads(resp.text)
+    readydata = extractlcc.extract(apicall, choice)
 
-	# if the parsing function fails, dispaly an error, else display viz.html with data
-	if readydata == False:
-		return redirect('/error1')
-	else:
-		return render_template("viz.html", displaydata=readydata)
+    # if the parsing function fails, dispaly an error, else display viz.html with data
+    if readydata == False:
+        return redirect('/error1')
+    else:
+        return render_template("viz.html", displaydata=readydata)
 
 @app.route('/error1')
 def handleerror1():
-	return render_template("error1.html")
+    return render_template("error1.html")
 
 @app.route('/error2')
 def handlerror2():
-	return render_template("error2.html")
+    return render_template("error2.html")
 
 if __name__ == '__main__':
-	app.run(debug=True, port=8000, host='0.0.0.0')
+    app.run(debug=True, port=8000, host='0.0.0.0')
