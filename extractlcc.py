2195cb4944efa258390278a8e6a919ed0e33b68c
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2015-05-11 18:07:52 -0400

    cleaning up by getting rid of some debugging bits

--- extractlcc.py
+++ extractlcc.py
@@ -1,14 +1,12 @@
 import json
 import sys
 import os
-import pdb
 
 def extract(jsontxt, radiochoice):
 	
     # set default values of variables
     changed = []
     errorcheck = False
-    print "running extract"
 
     # parse data based on radio button selected. Returns error if it fails.
     try:
@@ -34,7 +32,6 @@ def extract(jsontxt, radiochoice):
         for line in originalJSON:
             tr = {'@KEY':'name', '@VALUE':'size'}
             changed.append({tr[k]: v for k, v in line.items()})
-            print "running change keys"
 
     # if error free, return use-ready json, else return False
     if errorcheck == True:
@@ -42,5 +39,4 @@ def extract(jsontxt, radiochoice):
         output = {"name": "content", "children": changed}
         return output
     else:
-        print('returning false')
         return False
