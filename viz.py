5cc5f8f8f416c2499c7f0d9ced8f92c36b3ea2b9
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2016-08-14 00:24:27 -0400

    campus dropdown now with wtforms

--- viz.py
+++ viz.py
@@ -8,10 +8,35 @@ import ast
 
 app = Flask(__name__)
 
+campuschoices = [('BB', 'Baruch'),
+                 ('BM', 'BMCC'),
+                 ('BX', 'Bronx CC'),
+                 ('BC', 'Brooklyn College'),
+                 ('CC', 'City College'),
+                 ('SI', 'CSI'),
+                 ('GC', 'Graduate Center'),
+                 ('GJ', 'School of Journalism'),
+                 ('CL', 'School of Law'),
+                 ('NC', 'Guttman'),
+                 ('HO', 'Hostos'),
+                 ('HC', 'Hunter'),
+                 ('JJ', 'John Jay'),
+                 ('KB', 'Kingsborough'),
+                 ('LG', 'LaGuardia'),
+                 ('LE', 'Lehman'),
+                 ('ME', 'Medgar Evers'),
+                 ('NY', 'City Tech'),
+                 ('QC', 'Queens College'),
+                 ('QB', 'Queensborough'),
+                 ('YC', 'York')
+                 ]
+
 class SearchForm(Form):
-    keywords = StringField('Search these terms', [
+    keywords = StringField('query', [
                validators.Length(max=200, message="length"), 
                validators.Regexp('^[ a-zA-Z]*$', message="regex")])
+    campus = SelectField('Campus', choices=campuschoices,
+               validators=[validators.Required(message="campus")])
 
 @app.route('/')
 def index():
@@ -23,67 +48,58 @@ def index():
 def submit():
     form = SearchForm(request.form)
 
-    def validate(form):
+    def validateform(form):
         if request.method == 'POST' and form.validate():
-            query = request.form['query']
+            global query
+            query = form.keywords.data 
             return True
         else:
+            print "returning false" 
             return form.errors['keywords'][0]
 
     # extract campus info from the form, otherwise extract from the session
+ 
     try:
-        campus = ast.literal_eval(request.form['campus'])
+        chosencampus = form.campus
+        print "loaded campus from form"
     except:
-        campus = session['campus']
+        chosencampus = session['campus']
+        print "loaded campus from sesssion"
         pass
     else:
-        correct_campus = set(["{'BB': 'Baruch'}",
-                              "{'BM': 'BMCC'}",
-                              "{'BX': 'Bronx CC'}",
-                              "{'BC': 'Brooklyn College'}",
-                              "{'CC': 'City College'}",
-                              "{'SI': 'CSI'}",
-                              "{'GC': 'Graduate Center'}",
-                              "{'GJ': 'School of Journalism'}",
-                              "{'CL': 'School of Law'}",
-                              "{'NC': 'Guttman'}",
-                              "{'HO': 'Hostos'}",
-                              "{'HC': 'Hunter'}",
-                              "{'JJ': 'John Jay'}",
-                              "{'KB': 'Kingsborough'}",
-                              "{'LG': 'LaGuardia'}",
-                              "{'LE': 'Lehman'}",
-                              "{'ME': 'Medgar Evers'}",
-                              "{'NY': 'City Tech'}",
-                              "{'QC': 'Queens College'}",
-                              "{'QB': 'Queensborough'}",
-                              "{'YC': 'York'}"
-                              ])
-        if not request.form['campus'] in correct_campus:
+        print "else firing"
+        print validateform(form)
+        if validateform(form) == "campus":
+            print "failed validation"
             return render_template("viz.html", displaydata={},
-                                   errordata=3, campus=campus)
+                                   errordata=3, campus=chosencampus)
         else:
-            session['campus'] = ast.literal_eval(request.form['campus'])
-    campus_code = list(campus)[0]
+            print "loaded campus into session"
+            session['campus'] = form.campus
+            pass
+
 
     # validate length and characters in query
-    if validate(form) == "length":
+    if validateform(form) == "length":
         return render_template("viz.html", displaydata={},
-                               errordata=4, campus=campus)
-    elif validate(form) == "regex":
+                               errordata=4, campus=chosencampus)
+    elif validateform(form) == "regex":
         return render_template("viz.html", displaydata={},
-                               errordata=2, campus=campus)
+                               errordata=2, campus=chosencampus)
     else:
-        query = request.form['query']
+        pass
 
-    # validate dropdown menu
+    # validaVte dropdown menu
     choice = request.form['type']
     correct_choices = set(['lcc', 'creationdate', 'topic'])
     if choice not in correct_choices:
         return render_template("viz.html", displaydata={},
-                               errordata=3, campus=campus)
+                               errordata=3, campus=chosencampus)
 
     # make an api request using the inserting the query variable in the url
+    campus_code = form.campus.data
+    print campus_code
+    print query
     resp = requests.get('http://onesearch.cuny.edu/PrimoWebServices'
                         '/xservice/search/brief?&institution={}&'
                         'query=any,contains,{}&query=facet_rtype,exact,'
@@ -99,12 +115,12 @@ def submit():
     # viz.html with data
     if readydata is False:
         return render_template("viz.html", displaydata={}, errordata=1,
-                               campus=campus)
+                               campus=chosencampus)
     else:
         return render_template("viz.html", displaydata=readydata,
-                               errordata=0, val=query, campus=campus)
+                               errordata=0, val=query, campus=chosencampus)
 
 app.secret_key = 'key goes here'
 
 if __name__ == '__main__':
-    app.run(port=8000, host='127.0.0.1')
+    app.run(port=8000, host='127.0.0.1', debug=True)
