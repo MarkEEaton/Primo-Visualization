fe50bd4360e48efb6b50acc8a4a5a83652eb2162
MarkEEaton <mark.eaton@kbcc.cuny.edu>
2016-08-28 05:10:14 -0400

    flake8

--- viz.py
+++ viz.py
@@ -3,14 +3,12 @@ from wtforms import *
 import requests
 import extractfromjson
 import json
-import re
-import ast
 from key import key
 
 app = Flask(__name__)
 
 
-facetchoices = [('lcc', 'Library of Congress Classification'), 
+facetchoices = [('lcc', 'Library of Congress Classification'),
                 ('creationdate', 'Creation Date'),
                 ('topic', 'Topic')]
 campuschoices = [('BB', 'Baruch'),
@@ -36,72 +34,78 @@ campuschoices = [('BB', 'Baruch'),
                  ('YC', 'York')
                  ]
 
-# set up wtforms class
+
 class SearchForm(Form):
+    """ set up wtforms class """
     keywords = StringField('query', [
-               validators.Length(max=200, message="length"), 
-               validators.Regexp('^[ a-zA-Z]*$', message="regex")])
+        validators.Length(max=200, message="length"),
+        validators.Regexp('^[ a-zA-Z]*$', message="regex")])
     campus = SelectField('Campus', choices=campuschoices,
-             validators=[validators.Required(message="campus")])
+                         validators=[validators.Required(message="campus")])
     facet = SelectField('Facet', choices=facetchoices,
-            validators=[validators.Required(message="facet")])
+                        validators=[validators.Required(message="facet")])
 
 
 @app.route('/')
 def index():
+    """ show the search form """
     form = SearchForm()
-    return render_template("landing.html", displaydata={}, errordata=0, form=form)
+    return render_template("landing.html", displaydata={}, errordata=0,
+                           form=form)
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    """ handle form data and display the results """
     form = SearchForm(request.form)
-        
+
     def validateform(form):
         """ validates the form, else returns error codes """
         if form.campus and form.campus.data == "None":
             del form.campus
         if request.method == 'POST' and form.validate():
             global query
-            query = form.keywords.data 
-            return 
+            query = form.keywords.data
+            return
         else:
             print form.errors.itervalues().next()
             return form.errors.itervalues().next()
-     
+
     def allvalidate():
         """ runs the validation; renders templates if validation fails """
-        if validateform(form) == None:
+        if validateform(form) is None:
             return False
         elif "length" in validateform(form):
             return render_template("viz.html", displaydata={},
                                    errordata=4, campus=chosencampusname)
-        elif "regex" in  validateform(form):
+        elif "regex" in validateform(form):
             return render_template("viz.html", displaydata={},
                                    errordata=2, campus=chosencampusname)
         elif ("facet" or "campus") in validateform(form):
             return render_template("viz.html", displaydata={},
                                    errordata=3, campus=chosencampusname)
         else:
-            return False 
-    
+            return False
+
     def managesession():
+        """ handles session data """
         global campus_code, chosencampusname
         if form.campus.data == "None":
             campus_code = session['campus_code']
             chosencampusname = session['chosencampusname']
             pass
-        else: 
+        else:
             campus_code = form.campus.data
             chosencampusname = dict(campuschoices).get(form.campus.data)
-            session['campus_code'] = campus_code 
+            session['campus_code'] = campus_code
             session['chosencampusname'] = chosencampusname
             pass
 
     managesession()
-    if allvalidate(): 
+    if allvalidate():
         return allvalidate()
-    else: pass
+    else:
+        pass
 
     # make an api request using the inserting the query variable in the url
     resp = requests.get('http://onesearch.cuny.edu/PrimoWebServices'
@@ -115,7 +119,6 @@ def submit():
     apicall = json.loads(resp.text)
     readydata = extractfromjson.extract(apicall, form.facet.data)
 
-
     # if the parsing function fails, dispaly an error, else display
     # viz.html with data
     if readydata is False:
@@ -126,7 +129,7 @@ def submit():
                                errordata=0, val=query,
                                campus=chosencampusname)
 
-app.secret_key = key 
+app.secret_key = key
 
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
