fe8fe5c293f53ff9213eac3fd5055c585c82c190
MarkEEaton <mark.eaton@kbcc.cuny.edu>
2016-08-28 16:44:11 -0400

    working version with submit() broken out into a separate module

--- viz.py
+++ viz.py
@@ -1,5 +1,6 @@
 from flask import (Flask, render_template, request, session)
 from wtforms import *
+import handlesubmit
 import requests
 import extractfromjson
 import json
@@ -59,79 +60,14 @@ def submit():
     """ handle form data and display the results """
     form = SearchForm(request.form)
 
-    def validateform(form):
-        """ validates the form, else returns error codes """
-        if form.campus and form.campus.data == "None":
-            del form.campus
-        if request.method == 'POST' and form.validate():
-            global query
-            query = form.keywords.data
-            return
-        else:
-            return form.errors.itervalues().next()
-
-    def allvalidate():
-        """ runs the validation; renders templates if validation fails """
-        if validateform(form) is None:
-            return False
-        elif "length" in validateform(form):
-            return render_template("viz.html", displaydata={},
-                                   errordata=4, campus=chosencampusname)
-        elif "regex" in validateform(form):
-            return render_template("viz.html", displaydata={},
-                                   errordata=2, campus=chosencampusname)
-        elif ("facet" or "campus") in validateform(form):
-            return render_template("viz.html", displaydata={},
-                                   errordata=3, campus=chosencampusname)
-        else:
-            return False
-
-    def managesession():
-        """ handles session data """
-        global campus_code, chosencampusname
-        if form.campus.data == "None":
-            campus_code = session['campus_code']
-            chosencampusname = session['chosencampusname']
-            pass
-        else:
-            campus_code = form.campus.data
-            chosencampusname = dict(campuschoices).get(form.campus.data)
-            session['campus_code'] = campus_code
-            session['chosencampusname'] = chosencampusname
-            pass
-
-    def makeapicall():
-        """ make the api call and pass the data to extract() """
-
-        # make an api request using the inserting the query variable in the url
-        resp = requests.get('http://onesearch.cuny.edu/PrimoWebServices'
-                            '/xservice/search/brief?&institution={}&'
-                            'query=any,contains,{}&query=facet_rtype,exact,'
-                            'books&indx=1&loc=local,scope:'
-                            '(KB,AL,CUNY_BEPRESS)&'
-                            'loc=adaptor,primo_central_multiple_fe'
-                            '&json=true'.format(campus_code, query))
-
-        # assign the api data to a variable, pass it to the parsing function
-        apicall = json.loads(resp.text)
-        readydata = extractfromjson.extract(apicall, form.facet.data)
-
-        # if the parsing function fails, dispaly an error, else display
-        # viz.html with data
-        if readydata is False:
-            return render_template("viz.html", displaydata={}, errordata=1,
-                                   campus=chosencampusname)
-        else:
-            return render_template("viz.html", displaydata=readydata,
-                                   errordata=0, val=query,
-                                   campus=chosencampusname)
-
-    managesession()
-    if allvalidate():
-        return allvalidate()
+    handlesession = handlesubmit.managesession(form, campuschoices)
+    campus_code = handlesession[0]
+    chosencampusname = handlesession[1]
+    val = handlesubmit.allvalidate(form, campus_code, chosencampusname)
+    if type(val) == list:
+        return handlesubmit.makeapicall(campus_code, val[0], form, chosencampusname)
     else:
-        pass
-    return makeapicall()
+	return val 
 
 app.secret_key = key
 
