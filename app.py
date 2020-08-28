1825805578dc15c10506737e8262aa0d3a89079e
Mark Eaton <MarkEEaton@users.noreply.github.com>
2020-08-28 16:56:52 -0400

    add security headers; blacken

--- app.py
+++ app.py
@@ -10,82 +10,116 @@ app = Flask(__name__)
 
 class SearchForm(Form):
     """ set up wtforms class """
-    facet_choices = [('lcc', 'Library of Congress Classification'),
-                     ('creationdate', 'Creation Date'),
-                     ('topic', 'Topic')]
 
-    college_choices = [('BB', 'Baruch'),
-                       ('BM', 'BMCC'),
-                       ('BX', 'Bronx CC'),
-                       ('BC', 'Brooklyn College'),
-                       ('CC', 'City College'),
-                       ('NY', 'City Tech'),
-                       ('SI', 'CSI'),
-                       ('GC', 'Graduate Center'),
-                       ('NC', 'Guttman'),
-                       ('HO', 'Hostos'),
-                       ('HC', 'Hunter'),
-                       ('JJ', 'John Jay'),
-                       ('KB', 'Kingsborough'),
-                       ('LG', 'LaGuardia'),
-                       ('LE', 'Lehman'),
-                       ('ME', 'Medgar Evers'),
-                       ('QC', 'Queens College'),
-                       ('QB', 'Queensborough'),
-                       ('GJ', 'School of Journalism'),
-                       ('CL', 'School of Law'),
-                       ('YC', 'York')]
+    facet_choices = [
+        ("lcc", "Library of Congress Classification"),
+        ("creationdate", "Creation Date"),
+        ("topic", "Topic"),
+    ]
 
-    college = SelectField('college',
-                          choices=college_choices,
-                          validators=[validators.DataRequired(message='You must select a campus.')])
-    facet = SelectField('facet',
-                        choices=facet_choices,
-                        validators=[validators.DataRequired(message='You must select a facet.')])
-    keyword = StringField('keyword', [
-        validators.Length(max=200, message='You cannot enter more than 200 characters.'),
-        validators.Regexp(r'^[\-a-zA-Z ]*$',
-                          message='Invalid characters in your search string. \
-                                   Use only A-Z, -, and space.'),
-        validators.DataRequired(message='You must type in something.')])
+    college_choices = [
+        ("BB", "Baruch"),
+        ("BM", "BMCC"),
+        ("BX", "Bronx CC"),
+        ("BC", "Brooklyn College"),
+        ("CC", "City College"),
+        ("NY", "City Tech"),
+        ("SI", "CSI"),
+        ("GC", "Graduate Center"),
+        ("NC", "Guttman"),
+        ("HO", "Hostos"),
+        ("HC", "Hunter"),
+        ("JJ", "John Jay"),
+        ("KB", "Kingsborough"),
+        ("LG", "LaGuardia"),
+        ("LE", "Lehman"),
+        ("ME", "Medgar Evers"),
+        ("QC", "Queens College"),
+        ("QB", "Queensborough"),
+        ("GJ", "School of Journalism"),
+        ("CL", "School of Law"),
+        ("YC", "York"),
+    ]
 
-@app.route('/', methods=['GET', 'POST'])
+    college = SelectField(
+        "college",
+        choices=college_choices,
+        validators=[validators.DataRequired(message="You must select a campus.")],
+    )
+    facet = SelectField(
+        "facet",
+        choices=facet_choices,
+        validators=[validators.DataRequired(message="You must select a facet.")],
+    )
+    keyword = StringField(
+        "keyword",
+        [
+            validators.Length(
+                max=200, message="You cannot enter more than 200 characters."
+            ),
+            validators.Regexp(
+                r"^[\-a-zA-Z ]*$",
+                message="Invalid characters in your search string. \
+                                   Use only A-Z, -, and space.",
+            ),
+            validators.DataRequired(message="You must type in something."),
+        ],
+    )
+
+
+@app.after_request
+def add_security_headers(resp):
+    resp.headers["X-Content-Type-Options"] = "nosniff"
+    resp.headers["X-Frame-Options"] = "SAMEORIGIN"
+    resp.headers["X-XSS-Protection"] = "1; mode=block"
+    resp.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+    return resp
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     """ display the index page """
     form = SearchForm(request.form)
     faux_data = {"name": "content", "children": None}
 
-    if request.method == 'GET':
-        return render_template('index.html', error_message='', final_data=faux_data, form=form)
+    if request.method == "GET":
+        return render_template(
+            "index.html", error_message="", final_data=faux_data, form=form
+        )
 
     else:
         if form.validate():
-            get_facet = request.form['facet']
-            get_college = request.form['college']
-            get_keyword = request.form['keyword']
+            get_facet = request.form["facet"]
+            get_college = request.form["college"]
+            get_keyword = request.form["keyword"]
             print("---->        Facet: " + get_facet)
             print("---->        College: " + get_college)
             print("---->        Terms: " + get_keyword)
-            resp = requests.get('https://api-na.hosted.exlibrisgroup.com/primo/v1/'
-                                'search?vid=CUNY&scope=everything'
-                                '&q=any,contains,{}&qInclude=facet_local3,exact,{}'
-                                '&qInclude=facet_rtype,exact,books'
-                                '&apikey={}'.format(get_keyword, get_college, apikey))
-
+            resp = requests.get(
+                "https://api-na.hosted.exlibrisgroup.com/primo/v1/"
+                "search?vid=CUNY&scope=everything"
+                "&q=any,contains,{}&qInclude=facet_local3,exact,{}"
+                "&qInclude=facet_rtype,exact,books"
+                "&apikey={}".format(get_keyword, get_college, apikey)
+            )
 
             api_call = json.loads(resp.text)
 
             # what if there are no results?
-            if api_call['facets'] == []:
+            if api_call["facets"] == []:
                 error_message = "<div class='alert alert-danger' role='alert'>No results found!</div>"
-                return render_template('index.html', error_message=error_message,
-                                       final_data=faux_data, form=form)
+                return render_template(
+                    "index.html",
+                    error_message=error_message,
+                    final_data=faux_data,
+                    form=form,
+                )
 
             # parse the api data
-            facet_data = api_call['facets']
+            facet_data = api_call["facets"]
             for facet in facet_data:
-                if facet['name'] == get_facet:
-                    chosen_facet = facet['values']
+                if facet["name"] == get_facet:
+                    chosen_facet = facet["values"]
 
             # make chosen_facet a list if it is not a list
             # this will happen if there is only one result
@@ -95,29 +129,38 @@ def index():
             # transform the data into a format d3 will like
             transformed_facet = []
             for line in chosen_facet:
-                temp = {'value': 'name', 'count': 'size'}
+                temp = {"value": "name", "count": "size"}
                 transformed_facet.append({temp[k]: v for k, v in line.items()})
 
             final_data = {"name": "content", "children": transformed_facet}
 
-            return render_template("index.html", error_message='',
-                                   final_data=final_data, form=form)
+            return render_template(
+                "index.html", error_message="", final_data=final_data, form=form
+            )
         else:
             try:
-                error_text = form.errors['keyword'][0]
+                error_text = form.errors["keyword"][0]
             except:
                 try:
-                    error_text = form.errors['facet'][0]
+                    error_text = form.errors["facet"][0]
                 except:
                     try:
-                        error_text = form.errors['college'][0]
+                        error_text = form.errors["college"][0]
                     except:
-                        error_text = 'Unexpected error'
+                        error_text = "Unexpected error"
             finally:
-                error_message = "<div class='alert alert-danger' role='alert'>" + error_text + "</div>"
-                return render_template('index.html', error_message=error_message,
-                                       final_data=faux_data, form=form)
+                error_message = (
+                    "<div class='alert alert-danger' role='alert'>"
+                    + error_text
+                    + "</div>"
+                )
+                return render_template(
+                    "index.html",
+                    error_message=error_message,
+                    final_data=faux_data,
+                    form=form,
+                )
 
 
-if __name__ == '__main__':
-    app.run(port=8000, host='127.0.0.1', debug=True)
+if __name__ == "__main__":
+    app.run(port=8000, host="127.0.0.1", debug=True)
